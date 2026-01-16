faf1bff5c552471f4ab665624eabbeba6f608444
Zeeshan Akhtar <zeeshanakhtaransari0@gmail.com>
2026-01-17 00:59:31 +0530

    fix ResidentComplaints eslint build issue

--- frontend/src/pages/ResidentComplaints.js
+++ frontend/src/pages/ResidentComplaints.js
@@ -32,6 +32,7 @@ export default function ResidentComplaints() {
 
   useEffect(() => {
     fetchComplaints();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function fetchComplaints() {
